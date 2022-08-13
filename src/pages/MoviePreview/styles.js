4248bf9b9788b16fe7ad593a135c4aa6510d1cf4
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-13 11:01:24 -0300

    adicionando scroll no container

--- src/pages/MoviePreview/styles.js
+++ src/pages/MoviePreview/styles.js
@@ -4,9 +4,25 @@ export const Container = styled.div`
   width: 100%;
   > main {
     max-width: 1137px;
+    height: 581px;
     text-align: justify;
     margin-top: 2.5rem;
     margin-inline: auto;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+      width: 0.5rem;
+    }
+
+    &::-webkit-scrollbar-button {
+      height: 1rem;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-inline: 0.8rem solid ${({ theme }) => theme.COLORS.ROSE};
+      border-radius: 1rem;
+      background-clip: padding-box;
+    }
 
     color: ${({ theme }) => theme.COLORS.GRAY_100};
 
